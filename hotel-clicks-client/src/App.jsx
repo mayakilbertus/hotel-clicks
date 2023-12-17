437ca04c261667ef546447bddcbadf49af611a91
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-17 17:51:05 +0100

    graph setup

--- hotel-clicks-client/src/App.jsx
+++ hotel-clicks-client/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, Label } from "recharts";
+
+const url = "http://localhost:5005";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hotelData, setHotelData] = useState(null);
+
+  const fetchHotelData = () => {
+    axios
+      .get(`${url}/api/hotel`)
+      .then((response) => {
+        setHotelData(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log("Fetching Hotel data not successful", err);
+      });
+  };
+
+  useEffect(() => {
+    fetchHotelData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="graph-container">
+        <h1>Klickübersicht Grand Hotel Musterstadt</h1>
+        {hotelData && (
+          <LineChart
+            width={1500}
+            height={300}
+            data={hotelData}
+            className="chart"
+          >
+            <XAxis dataKey="date" />
+            <YAxis dataKey="numOfClicks">
+              <Label
+                angle={-90}
+                position="insideLeft"
+                style={{ textAnchor: "middle" }}
+              >
+                Anzahl der Klicks
+              </Label>
+            </YAxis>
+            <CartesianGrid stroke="#8e918f" strokeDasharray="5 5" />
+            <Line type="monotone" dataKey="numOfClicks" stroke="#8884d8" />
+          </LineChart>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
