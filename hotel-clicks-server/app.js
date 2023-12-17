1d52186d9264080e4b8ec640633641ca8a61f05c
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-17 17:52:25 +0100

    setting up CORS

--- hotel-clicks-server/app.js
+++ hotel-clicks-server/app.js
@@ -1,10 +1,10 @@
 require("./db");
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-
+const cors = require("cors");
 const Hotel = require("./models/Hotel.model");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/hotel", (req, res, next) => {
